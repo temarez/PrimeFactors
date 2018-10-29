a73ed301525be4ed3304f63a813ff46468dbd3cc
Artem Rieznikov <temarez@gmail.com>
2018-10-29 17:58:43 +0200

    Step 3 - will fail

--- src/PrimeFactors/PrimeFactorsTest.kt
+++ src/PrimeFactors/PrimeFactorsTest.kt
@@ -9,12 +9,20 @@ class PrimeFactorsTest {
     @Test fun factors() {
         assertThat(factorsOf(1), `is`(emptyList()))
         assertThat(factorsOf(2), `is`(listOf(2)))
+        assertThat(factorsOf(3), `is`(listOf(3)))
+        assertThat(factorsOf(4), `is`(listOf(2, 2)))
     }
 
-    private fun factorsOf(n: Int): List<Int> {
+    private fun factorsOf(nn: Int): List<Int> {
+        var n = nn
         val factors = mutableListOf<Int>()
-        if (n>1)
-            factors.add(2)
+        if (n>1) {
+            if (n%2 == 0) {
+                factors.add(2)
+                n /= 2
+            }
+            factors.add(n)
+        }
         return factors
     }
 }
